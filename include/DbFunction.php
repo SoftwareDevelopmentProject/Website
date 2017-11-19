61c6b79c690e652dccdc1c1e1a573ecb69f01387
khaibin <bkchow@yes.my>
2017-11-19 14:50:43 +0800

    hash passwords

--- include/DbFunction.php
+++ include/DbFunction.php
@@ -20,9 +20,11 @@ class DbFunction {
     public function register($name, $email, $password, $phone, $address, $country){
         $db = new DbConnect();
         $con = $db->connect();
+        $password = password_hash($password, PASSWORD_DEFAULT);
         $register = mysqli_query($con, "INSERT INTO member(member_name, member_email, member_password, member_phone, member_address, member_country) VALUES ('$name','$email', '$password','$phone','$address','$country')");
         return $register;
     }
+
 
 }
 
