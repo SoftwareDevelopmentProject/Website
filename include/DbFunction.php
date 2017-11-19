ac69d26be687d2e9e9761f3737c04e4bf9e4344f
khaibin <bkchow@yes.my>
2017-11-19 14:28:15 +0800

    member register: fix query

--- include/DbFunction.php
+++ include/DbFunction.php
@@ -7,24 +7,20 @@ include_once 'DbConnect.php';
 class DbFunction {
 
     public function getBooks() {
-
         $db = new DbConnect();
         $con = $db->connect();
-
         $books = array();
         $result = mysqli_query($con, "SELECT * FROM book");
         while ($book = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
             array_push($books, $book);
         }
         return $books;
-
     }
-    public function register($name, $email, $password, $phone, $address,$country){
+
+    public function register($name, $email, $password, $phone, $address, $country){
         $db = new DbConnect();
         $con = $db->connect();
-        $register = mysqli_query($con, "INSERT INTO member(member_name, member_email, member_password, member_phone, member_address, member_country) 
-VALUES
-('$name','$email', '$password','$phone','$address') ");
+        $register = mysqli_query($con, "INSERT INTO member(member_name, member_email, member_password, member_phone, member_address, member_country) VALUES ('$name','$email', '$password','$phone','$address','$country')");
         return $register;
     }
 
