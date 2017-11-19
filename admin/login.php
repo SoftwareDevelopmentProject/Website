c583c9d7f70034cfe57759a91a6d3a5e2e56ae6a
khaibin <bkchow@yes.my>
2017-11-19 13:16:12 +0800

    admin: seperated header and footer

--- admin/login.php
+++ admin/login.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Lumino - Login</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/datepicker3.css" rel="stylesheet">
-	<link href="css/styles.css" rel="stylesheet">
-	<!--[if lt IE 9]>
-	<script src="js/html5shiv.js"></script>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
+    <?php include_once '_head.php'; ?>
 </head>
 <body>
 	<div class="row">
