d4cd70e72644c4e69595acee1e384d17826b29d3
khaibin <bkchow@yes.my>
2017-11-25 17:25:39 +0800

    security: lock account after multiple fail login attempts

--- include/Config.php
+++ include/Config.php
@@ -6,9 +6,14 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'sdp');
 
+// Security options
+define('SECURITY_MAX_LOGIN_TIMEOUT', 360); // 360 minutes
+define('SECURITY_MAX_LOGIN_ATTEMPT', 6);
+
 // Login
 define('LOGIN_USER_NOT_FOUND', 0);
 define('LOGIN_SUCCESS', 1);
-define('LOGIN_PASSWORD_INCORRECT', 2)
+define('LOGIN_PASSWORD_INCORRECT', 2);
+define('LOGIN_ACCOUNT_LOCKED', 3);
 
 ?>
--- include/DbFunction.php
+++ include/DbFunction.php
@@ -31,17 +31,35 @@ class DbFunction {
         $result = mysqli_query($con, "SELECT * FROM member WHERE member_email = '$email'");
         if (mysqli_num_rows($result) > 0) {
             $member = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($this->isAccountLocked($member['member_id'])) {
+                return LOGIN_ACCOUNT_LOCKED;
+            }
             if (password_verify($password, $member['member_password'])) {
                 $_SESSION['user'] = $email;
                 $_SESSION['password'] = md5($member['member_password']);
+                $this->recordLoginAttempt($member['member_id'], true);
                 return LOGIN_SUCCESS;
-            } else {
-                return LOGIN_PASSWORD_INCORRECT;
             }
-        } else {
-            return LOGIN_USER_NOT_FOUND;
+            $this->recordLoginAttempt($member['member_id'], false);
+            return LOGIN_PASSWORD_INCORRECT;
         }
+        return LOGIN_USER_NOT_FOUND;
     }
+
+    public function recordLoginAttempt($member_id, $status) {
+        $db = new DbConnect();
+        $con = $db->connect();
+        $status = (int) $status;
+        mysqli_query($con, "INSERT INTO login_attempt(member_id, status) VALUES ($member_id, $status)");
+    }
+
+    public function isAccountLocked($member_id) {
+        $db = new DbConnect();
+        $con = $db->connect();
+        $result = mysqli_query($con, "SELECT * FROM login_attempt WHERE member_id = $member_id AND status = 0 AND created_at > NOW() - INTERVAL " . SECURITY_MAX_LOGIN_TIMEOUT . " MINUTE");
+        return !(mysqli_num_rows($result) < SECURITY_MAX_LOGIN_ATTEMPT);
+    }
+
 	public function up_staff($id, $role){
         $db = new DbConnect();
         $con = $db->connect();
