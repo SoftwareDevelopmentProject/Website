fe98d34f70c1d0fdfbb73539a71b39ef055e3e8a
liyi98 <ly19980804@gmail.com>
2017-11-26 18:01:48 +0800

    add contact about pages

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Free Adidas Website Template | Checkout :: w3layouts</title>
+<?php include"_head.php";?>
+</head>
+<body>
+ <?php include "_header.php";?>
+       <div class="login">
+         <div class="wrap">
+    	     <h4 class="title">Shopping cart is empty</h4>
+    	     <p class="cart">You have no items in your shopping cart.<br>Click<a href="index.php"> here</a> to continue shopping</p>
+    	   </div>
+		</div>
+       <?php include "_footer.php"?>
+</body>
+</html>
