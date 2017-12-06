7ad41dec4427700afd1787418379649f95b4b2a1
khaibin <bkchow@yes.my>
2017-12-06 22:02:18 +0800

    Fix email sending

--- admin/_email.php
+++ admin/_email.php
@@ -3,23 +3,44 @@
 	$subject = "Account created";
 	$ps= $_POST['phone'];
 	$link = "http://localhost:8080/SDP/Website/admin/login.php";
-	$headers = "From: Technology Park's Bookstore";
-	
-	
-	require_once "PHPMailer/PHPMailerAutoload.php";
-	$mail = new PHPMailer;
-	#$mail->SMTPDebug = 4; 
-	$mail->isSMTP();
-	$mail->Host = 'smtp.gmail.com';
-	$mail->SMTPAuth = true;
-	$mail->Username = 'technologyparkbookstore@gmail.com';
-	$mail->Password = 'Liyi<3Khaibin';
-	$mail->SMTPSecure = 'tls';
-	$mail->Port = 587;
 
-	$mail->setFrom("technologyparkbookstore@gmail.com", "Technology Park's Bookstore");
-	$mail->addAddress($email);
-	$mail->Subject = $subject;
-	$mail->Body    = "Welcome to join as Technology Park's Bookstore staff. Your staff account is just been created and your password would be your phone number. Click the link below to login! \n".$link;
-	$mail->send();
-			?>
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../include/PHPMailer/src/Exception.php';
+require '../include/PHPMailer/src/PHPMailer.php';
+require '../include/PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 3;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'technologyparkbookstore@gmail.com';                 // SMTP username
+    $mail->Password = 'Liyi<3Khaibin';                           // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        ));
+
+    //Recipients
+    $mail->setFrom('technologyparkbookstore@gmail.com', 'Technology Park\'s Bookstore');
+    $mail->addAddress($email);               // Name
+    //Attachments                               // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = 'Welcome to join as Technology Park\'s Bookstore staff. Your staff account is just been created and your password would be your phone number. Click the link below to login! \n'.$link;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
+?>
