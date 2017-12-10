3f2655678abc0cc62e994286bbc808264514ab28
liyi98 <ly19980804@gmail.com>
2017-12-10 23:24:12 +0800

    About us

--- about.php
+++ about.php
@@ -14,8 +14,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
  <?php include "_header.php";?>
        <div class="login">
          <div class="wrap">
-    	     <h4 class="title">Shopping cart is empty</h4>
-    	     <p class="cart">You have no items in your shopping cart.<br>Click<a href="index.php"> here</a> to continue shopping</p>
+    	  <h1>About Us</h1>
+
     	   </div>
 		</div>
        <?php include "_footer.php"?>
