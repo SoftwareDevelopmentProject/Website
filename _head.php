931687124d5f14d5deac2e2274e804cdc3b25037
liyi98 <ly19980804@gmail.com>
2018-01-14 21:49:11 +0800

    cart cart

--- _head.php
+++ _head.php
@@ -7,6 +7,8 @@ session_start();
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="admin/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+<link href="admin/css/bootstrap-table.css" rel="stylesheet" type="text/css" media="all" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript">
--- cart.php
+++ cart.php
@@ -13,31 +13,55 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <body>
 <?php include "_header.php";?>
 <div class="login">
-    <div class="wrap">
-        <table style="border: 1px;width: 80%;">
+    <div class="wrap" style="margin-bottom: 50px;">
+        <?php if (isset($_SESSION['cart'])) {?>
+        <table class="table">
             <tr>
-                <td>Image</td>
-                <td>Price</td>
-                <td>Quantity</td>
-                <td>Subtotal</td>
+                <th class="fixed-table-header">Image</th>
+                <th class="fixed-table-header">Book Name</th>
+                <th class="fixed-table-header">Price</th>
+                <th class="fixed-table-header">Quantity</th>
+                <th class="fixed-table-header">Subtotal</th>
             </tr>
             <?php
-            if (isset($_SESSION['cart'])) {
+
                 $cart = $db->viewCart();
+                $total = 0;
                 foreach ($cart as $book) {
                     $subtotal = $book['book_price'] * $book['quantity'] ;
+                    $total+=$subtotal;
                     echo '<tr>
                   <td><img src="images/products/' . $book['book_id'] . '.jpg" alt=""/></td>
+                  <td>' . $book['book_title'] . '</td>
                   <td>' . $book['book_price'] . '</td>
                   <td>' . $book['quantity'] . '</td>
                   <td>'.$subtotal.'</td>
                   <td></td>
-              </tr>';
+              </tr> '
+                    ;
 
                 }
-            }
+
+
+
             ?>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Total</td>
+                <td><?php echo $total; ?></td>
+
+            </tr>
         </table>
+        <div class="" style="margin-top: 10px;">
+            <button class="btn btn-primary btn-sm" style="float: right;margin-left: 20px;">Checkout</button>
+            <button class="btn btn-primary btn-sm" style="float: right;" onclick="">Clear</button>
+        </div>
+        <?php }else{
+            echo'<h4 class="title">Shopping cart is empty</h4>
+    	     <p class="cart">You have no items in your shopping cart.<br>Click<a href="index.php"> here</a> to continue shopping</p>';
+        }?>
     </div>
 </div>
 <?php include "_footer.php"?>
