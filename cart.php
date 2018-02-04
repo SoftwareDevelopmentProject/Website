145663b4ca0ec85fde1a519cd6b46dd6fa1752c6
liyi98 <ly19980804@gmail.com>
2018-02-04 16:19:26 +0800

    modify cart page

--- cart.php
+++ cart.php
@@ -19,21 +19,30 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <?php if(isset($_POST['clear'])){
     $db->setSession();
     header("Refresh:0");
+}
+
+if(isset($_POST['delete'])){
+    $db->delCart($_POST['delete']);
+    header("Refresh:0");
 }?>
 <body>
 <?php include "_header.php";?>
 <div class="login">
     <div class="wrap" style="margin-bottom: 50px;">
-        <?php if (sizeof($_SESSION['cart']) > 0) {?>
+        <?php if (sizeof($_SESSION['cart']) > 0  ){?>
         <table class="table">
-            <tr>
-                <th class="fixed-table-header">Image</th>
-                <th class="fixed-table-header">Book Name</th>
-                <th class="fixed-table-header">Price(RM)</th>
-                <th class="fixed-table-header">Quantity</th>
-                <th class="fixed-table-header">Subtotal</th>
+            <thead style="text-align: center;">
+            <tr style="font-weight: bold;">
+                <th class="fixed-table-header" style="text-align: center"></th>
+                <th class="fixed-table-header" style="text-align: center">Image</th>
+                <th class="fixed-table-header" style="text-align: center">Book Name</th>
+                <th class="fixed-table-header" style="text-align: center">Price(RM)</th>
+                <th class="fixed-table-header" style="text-align: center">Quantity</th>
+                <th class="fixed-table-header" style="text-align: center">Subtotal</th>
+                <th class="fixed-table-header" style="text-align: center"></th>
 
             </tr>
+            </thead>
             <?php
 
                 $cart = $db->viewCart();
@@ -42,12 +51,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                     $subtotal = $book['book_price'] * $book['quantity'] ;
                     $total+=$subtotal;
                     echo '<tr>
-                  <td><img src="images/products/' . $book['book_id'] . '.jpg" alt=""/></td>
-                  <td>' . $book['book_title'] . '</td>
-                  <td>' . $book['book_price'] . '</td>
-                  <td>' . $book['quantity'] . '</td>
-                  <td>'.$subtotal.'</td>
-                  <td></td>
+                  <td style="text-align: center"><input type="checkbox"/></td>
+                  <td style="width: 220px;text-align: center"><img src="images/products/' . $book['book_id'] . '.jpg" alt=""/></td>
+                  <td style="text-align: center">' . $book['book_title'] . '</td>
+                  <td style="text-align: center">' . $book['book_price'] . '</td>
+                  <td style="text-align: center">' . $book['quantity'] . '</td>
+                  <td style="text-align: center">'.$subtotal.'</td>
+                  <td style="text-align: center"><form method="post" onclick="this.submit();"> <input type="hidden" name="delete" value="' . $book['book_id'] . '"><i class="fa fa-trash"></i></form></td>
               </tr> '
                     ;
 
@@ -56,12 +66,14 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 
             ?>
-            <tr>
+            <tr style="font-weight: bold;">
+                <td ></td>
                 <td></td>
                 <td></td>
                 <td></td>
-                <td>Total</td>
-                <td><?php echo $total; ?></td>
+                <td style="text-align: center">Total</td>
+                <td style="text-align: center"><?php echo $total; ?></td>
+                <td></td>
 
             </tr>
         </table>
@@ -72,7 +84,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
             <button name="clear" class="btn btn-primary btn-sm" style="float: right;">Clear</button>
             </form>
         </div>
-        <?php }else{
+        <?php }else if(!isset($_SESSION['cart'])){
+            echo'<h4 class="title">Shopping cart is empty</h4>
+    	     <p class="cart">You have no items in your shopping cart.<br>Click<a href="index.php"> here</a> to continue shopping</p>';
+            }else{
             echo'<h4 class="title">Shopping cart is empty</h4>
     	     <p class="cart">You have no items in your shopping cart.<br>Click<a href="index.php"> here</a> to continue shopping</p>';
         }?>
