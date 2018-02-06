4988bf0c50b51cfb12163989100a15df0d05dd3d
khaibin <bkchow@yes.my>
2018-02-06 12:16:32 +0800

    Member reward points completed

--- checkout.php
+++ checkout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 ?>
 <!--A Design by W3layouts
 Author: W3layout
@@ -10,67 +11,88 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Register</title>
-    <?php include "_head.php";?>
+    <title>Free Adidas Website Template | Checkout :: w3layouts</title>
+    <?php include"_head.php";?>
+    <link href="admin/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="admin/css/bootstrap-table.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 
 <body>
 <?php include "_header.php";?>
+<div class="login">
+    <div class="wrap" style="margin-bottom: 50px;">
+            <form method="post" action="payment.php">
+        <table class="table">
+            <thead style="text-align: center;">
+            <tr style="font-weight: bold;">
+                <th class="fixed-table-header" style="text-align: center">Image</th>
+                <th class="fixed-table-header" style="text-align: center">Book Name</th>
+                <th class="fixed-table-header" style="text-align: center">Price(RM)</th>
+                <th class="fixed-table-header" style="text-align: center">Quantity</th>
+                <th class="fixed-table-header" style="text-align: center">Subtotal</th>
+
+            </tr>
+            </thead>
+            <?php
+            $total = 0;
+            $checkout = array();
+            if (count($_POST['book']) <= 0) {
+                echo "<script>window.location.replace('cart.php')</script>";
+            }
+                foreach ($_POST['book'] as $book_id) {
+                    $book = $db->getBook($book_id);
+                    $quantity = $db->cartgetQuantity($book_id);
+                    $subtotal = $book['book_price'] * $quantity;
+                    $total += $subtotal;
+                    $c = array();
+                    $c['book_id'] = $book_id;
+                    $c['quantity'] = $quantity;
+                    array_push($checkout, $c);
+                    ?>
+                <tr>
+                  <td style="width: 220px;text-align: center"><img src="images/products/<?php echo $book_id;?>.jpg" alt=""/></td>
+                  <td style="text-align: center"><?php echo $book['book_title'];?></td>
+                  <td style="text-align: center"><?php echo $book['book_price'];?></td>
+                  <td style="text-align: center"><?php echo $quantity;?></td>
+                  <td style="text-align: center"><?php echo $subtotal;?></td>
+              </tr>
+
+
 <?php
-if($user == null){
-    echo'<script>alert("Please login before checkout")</script>';
-    echo '<script>window.location.replace("login.php")</script>';
-}else{
-    if(isset($_POST['checkoutdetails'])) {
-        $transaction_id = rand(10000, 99999);
-        $order_id = $db->checkout($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['address'], $_POST['pm'], $transaction_id, $_SESSION['user']);
-        $bookarray = json_decode($_POST['book']);
-        foreach ($bookarray as $book){
-            $qty = $db->cartgetQuantity($book);
-            $db->insertOrderdetails($order_id, $book, $qty);
-            $db->delCart($book);
-        }
-        echo '<script>alert("Checkout Sucessfully!!")</script>';
-        echo '<script>window.location.replace("index.php")</script>';
+                }
 
-    }
+            ?>
+            <tr style="font-weight: bold;">
+                <td style="text-align: center;">Apply Discount</td>
+                <td>
+                    <select name="discount" onchange="document.getElementById('cartTotal').innerHTML = (<?php echo $total; ?> - parseInt(this.value)).toFixed(2)">
+                        <option value="0">No discount</option>
+                        <option value="5" <?php if(($user['member_reward_points'] < 100) || ($total < 5)) echo 'disabled'; ?>>RM 5 (100 points)</option>
+                        <option value="20" <?php if(($user['member_reward_points'] < 300) || ($total < 20)) echo 'disabled'; ?>>RM 20 (300 points)</option>
+                        <option value="50" <?php if(($user['member_reward_points'] < 500) || ($total < 50)) echo 'disabled'; ?>>RM 50 (500 points)</option>
+                    </select>
+                </td>
+                <td></td>
+                <td style="text-align: right">Total</td>
+                <td style="text-align: center" id="cartTotal"><?php echo $total; ?></td>
 
-    ?>
-<div class="register_account" style="margin-bottom: 50px;">
-    <div class="wrap">
-        <h4 class="title">Checkout </h4>
-        <form method="post" style="width: 100%" >
-            <center>
-            <div class="col_1_of_1 span_1_of_1" style="margin: auto">
-                <input type="hidden" name="book" value='<?php echo json_encode($_POST['book']);?>'>
-                <div><label>RecipentName</label></div><div><input type="text" name ="name" style="width: 50%;"></div>
-                <div><label>RecipentEmail</label></div><div><input type="text" name ="email" style="width: 50%;"></div>
-                <div><label>RecipentPhone</label></div><div><input type="text" name="phone" style="width: 50%; "></div>
-                <div><label>RecipentAddress</label></div><div><input type="text" name="address" style="width: 50%;"></div>
-                    <div><label>PaymentMethod</label></div><div><select name="pm" style="width: 50%;" onchange="if(this.value === 'CreditCard') document.getElementById('card').style.display = 'block'; else document.getElementById('card').style.display = 'none';">
-                        <option>Please Select</option>
-                        <option value="Cash">Delivery on Cash</option>
-                        <option value="CreditCard">Credit Card</option>
-                    </select></div>
-               <div style="display: none" id="card">
-                   <div><label>CreditCard Number</label></div>
-                   <div>
-                       <input type="text" name ="creditcard" placeholder="16DigitNumber" style="width: 31%;margin-right: 10px;">
-                       <input type="text" placeholder="CVV" style="width: 7%;margin-right: 10px;">
-                       <input type="text" placeholder="MM/YY" style="width: 10%;">
-                   </div>
-               </div>
-            </div>
+            </tr>
+        </table>
 
-            </center>
-            <div style="float: right"><input type="submit" name="checkoutdetails" class="grey" value="Continue" /></div>
-            <div class="clear"></div>
 
-            <p class="terms"></p>
-        </form>
+
+        <div class="" style="margin-top: 10px;">
+            <button name="payment" class="btn btn-primary btn-sm" style="float: right;margin-left: 20px;">Proceed to Payment</button>
+            <input type="hidden" value='<?php echo json_encode($checkout); ?>' name="checkout" />
+            </form>
+
+        </div>
     </div>
 </div>
-<?php include "_footer.php";?>
-<?php }?>
+<form id="deleteItem" method="post">
+    <input id="deleteItemId" name="delete" value="" type="hidden" />
+</form>
+
+<?php include "_footer.php"?>
 </body>
 </html>
